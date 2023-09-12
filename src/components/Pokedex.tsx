0e63541a2c34cc4912f4c3021bc6eb90d8f47b82
JoseRobertoGC <a01656294@tec.mx>
2023-09-12 14:02:42 -0600

    Cambios Jose

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,30 +1,29 @@
+import React, { useEffect, useState } from "react";
 import Busqueda from "./pokedex/Busqueda";
-import "./Pokedex.css"
+import "./Pokedex.css";
 import PokemonInfo from "./pokedex/PokemonInfo";
 import { Grid } from "@mui/material";
 import { Pokemon } from "../models/Pokemon";
-import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Pokedex() {
     const columnSize = 6;
-    const [pokemonId,setPokemonId]=useState(0);
-    const [pokemon,setPokemon]=useState<Pokemon|null>(null);
-    function selectPokemonId(id:number){
+    const [pokemonId, setPokemonId] = useState(0);
+    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+    function selectPokemonId(id: number) {
         setPokemonId(id);
     }
 
-    function getPokemon(){
-        axios.get("https://pokeapi.co/api/v2/pokemon/"+pokemonId).then((response)=>{
-            setPokemon(response.data)
-        })
+    function getPokemon() {
+        axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemonId).then((response) => {
+            setPokemon(response.data);
+        });
     }
-    
 
-    useEffect(()=>{
-        console.log("Cambio el valor de pokemonId y ahora es "+pokemonId)
-    },[pokemonId])
-
+    useEffect(() => {
+        console.log("Cambio el valor de pokemonId y ahora es " + pokemonId);
+    }, [pokemonId]);
 
     return (
         <div className="Pokedex">
@@ -33,14 +32,12 @@ function Pokedex() {
                     <Busqueda searchPokemon={getPokemon} changePokemonId={selectPokemonId}></Busqueda>
                 </Grid>
                 <Grid item xs={columnSize} className="Resultado">
-                    {pokemon&&
-                        <PokemonInfo pokemon={pokemon}></PokemonInfo>
-                    }
+                    {pokemon && <PokemonInfo pokemon={pokemon}></PokemonInfo>}
                 </Grid>
             </Grid>
         </div>
     );
-
 }
 
 export default Pokedex;
+
